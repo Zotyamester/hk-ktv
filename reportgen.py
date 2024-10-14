5dd69c3d81d97c4c27a11179ab4913c915a2e6fe
Zotyamester <szatmary.zoltan1222@gmail.com>
2024-10-14 19:48:27 +0200

    Add XLSX generation

--- reportgen.py
+++ reportgen.py
@@ -1,20 +1,23 @@
 import os
 import urllib.request
+import pandas as pd
 
 import PyPDF2
 
 # Source of input files: https://vik.bme.hu/page/62/
 
 paths = [
-        'https://vik.bme.hu/document/6098/original/5n_a7_2.pdf',
-        'https://vik.bme.hu/document/6100/original/5n_a7_4.pdf',
-        'https://vik.bme.hu/document/6099/original/5n_a7_6.pdf',
-        'https://vik.bme.hu/document/6101/original/5n_a8_2.pdf',
-        'https://vik.bme.hu/document/6102/original/5n_a8_4.pdf',
-        'https://vik.bme.hu/document/6108/original/5n_a8_6.pdf',
-        'https://vik.bme.hu/document/6103/original/5n_a9_2.pdf',
-        'https://vik.bme.hu/document/6104/original/5n_a9_4.pdf',
-        'https://vik.bme.hu/document/6118/original/szv_bsc.pdf'
+    'https://vik.bme.hu/document/6390/original/5na71.pdf',
+    'https://vik.bme.hu/document/6391/original/5na73.pdf',
+    'https://vik.bme.hu/document/6395/original/5na75.pdf',
+    'https://vik.bme.hu/document/6392/original/5na81.pdf',
+    'https://vik.bme.hu/document/6393/original/5na83.pdf',
+    'https://vik.bme.hu/document/6394/original/5na85.pdf',
+    'https://vik.bme.hu/document/6399/original/5na87.pdf',
+    'https://vik.bme.hu/document/6396/original/5na91.pdf',
+    'https://vik.bme.hu/document/6397/original/5na93.pdf',
+    'https://vik.bme.hu/document/6398/original/5na95.pdf',
+    # 'https://vik.bme.hu/document/6413/original/szvtx.pdf',
 ]
 
 # The work around to avoid using BeautifulSoup4 is to insert this
@@ -23,6 +26,12 @@ paths = [
 """
 $('.main-page-container a').each(function(index) { console.log($(this).attr('href'))});
 """
+
+COURSE_CODE = {
+    "7": "vill",
+    "8": "inf",
+    "9": "bprof",
+}
 
 # Only including workdays
 DAYS_OF_WEEK = {'Hétfő': 1, 'Kedd': 2,
@@ -64,8 +73,16 @@ def process_line(line):
     return row[:3] + [' '.join(row[3:neptun_code_idx])] + row[neptun_code_idx:neptun_code_idx + 3]
 
 
-def update_days(days, row):
+def update_days(days: pd.DataFrame, row):
     day_of_week, start_time, end_time, name, neptun_code, course_code, course_type = row
+
+    end_time_tokens = end_time.split(':')
+    end_time_as_minutes = int(end_time_tokens[0]) * 60 + int(end_time_tokens[1])
+
+    start_time_tokens = start_time.split(':')
+    start_time_as_minutes = int(start_time_tokens[0]) * 60 + int(start_time_tokens[1])
+
+    duration = (end_time_as_minutes - start_time_as_minutes + 59) // 60
 
     course_type = abbreviate_course_type(course_type)
 
@@ -74,25 +91,16 @@ def update_days(days, row):
         return
 
     event = create_event(name, course_type)
+    for i in range(duration):
+        start_time = '%02d:%02d' % (start_time_as_minutes // 60, 00)
+        days[day_of_week][start_time].append(event)
+        start_time_as_minutes += 60
 
-    day = days.get(day_of_week, {})
-    starting_at = day.get(start_time, set())
-    starting_at.add(event)
-    day[start_time] = starting_at
-    days[day_of_week] = day
-
-def print_days(days, f):
-    for day, schedule in sorted(days.items(), key=lambda kv: DAYS_OF_WEEK[kv[0]]):
-        print('\t' + day, file=f)
-        for start_time, event in sorted(schedule.items(), key=lambda kv: kv[0]):
-            print('\t\t' + start_time + ':' +
-                    '/'.join(event), file=f)
-
-def make_report(report_filename):
+def make_report(report_name):
     if not os.path.exists('pdfs'):
         os.mkdir('pdfs')
 
-    with open(report_filename, 'w', encoding='utf-8') as f:
+    with pd.ExcelWriter(f"{report_name}.xlsx") as writer:
         for path in paths:
             filename = os.path.basename(path)
             input_filename = os.path.join('pdfs', filename)
@@ -107,8 +115,8 @@ def make_report(report_filename):
                     print('SKIPPED %s' % filename)
                     continue
 
-                # Prepopulating the days, so that every weekday must appear when printing the dict out
-                days = {key: {} for key in DAYS_OF_WEEK}
+                # Make a pandas DataFrame with DAYS_OF_WEEK x hours (from 8 to 20)
+                days = pd.DataFrame({day: {('%02d:%02d' % (hour, 0)): [] for hour in range(8, 20)} for day in DAYS_OF_WEEK.keys()})
 
                 for page_num in range(pdf_reader.getNumPages()):
                     # Get the page object for the current page number
@@ -125,10 +133,20 @@ def make_report(report_filename):
                         row = process_line(line)
                         if len(row) >= 7:
                             update_days(days, row)
+                        else:
+                            print('SKIPPED: %s' % line)
 
-                print(filename, file=f)
-                print_days(days, f)
+                # Flatten the third dimension of the DataFrame by concatenating the strings (separated by a '/')
+                for day in days.columns:
+                    for hour in days.index:
+                        days[day][hour] = ' / '.join(set(days[day][hour]))
+
+                # Print DataFrame to Excel sheet
+                filename_without_extension = '.'.join(filename.split('.')[:-1])
+                course = COURSE_CODE[filename_without_extension[3]]
+                semester = filename_without_extension[4]
+                days.to_excel(writer, sheet_name=f"{course} {semester}")
 
 
 if __name__ == '__main__':
-    make_report('report.txt')
+    make_report('report')
